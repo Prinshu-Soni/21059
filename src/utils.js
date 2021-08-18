a096c77b33618aee6f0744ca9a6571fdf7f2cfc0
Prinshu-Soni <prinshu.soni@velotio.com>
2021-08-18 20:09:19 +0530

    fix indentation

--- src/utils.js
+++ src/utils.js
@@ -9,7 +9,7 @@ export function calculateWinner(cells) {
 		[0, 4, 8],
 		[2, 4, 6],
 	];
-    let winner;
+	let winner;
 	lines.forEach((line) => {
 		const [a, b, c] = line;
 		if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
